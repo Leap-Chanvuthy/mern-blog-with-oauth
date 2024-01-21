47a7fa4b7ff496b4e08397069808acac8c3adaef
Elon Thy <Chanvuthy.leap@student.cadt.edu.kh>
2024-01-21 19:24:49 +0700

    initialize frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h5 className='text-red-600'>Hello this is react </h5>
+    </>
+  )
+}
+
+export default App
