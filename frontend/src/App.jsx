80eaf09ee279cdc804724e05b6125d1ca727ab96
Elon Thy <Chanvuthy.leap@student.cadt.edu.kh>
2024-01-22 22:02:53 +0700

    finish: create header layout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ import Dashboard from './pages/Dashboard';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import About from './pages/About';
-import Header from './components/header';
+import Header from './components/Header';
 
 
 import { BrowserRouter , Routes , Route } from 'react-router-dom';
