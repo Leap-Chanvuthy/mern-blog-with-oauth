695dbd8d88d9cdd5649a5e5b3cf6b45b667d0c9c
Elon Thy <Chanvuthy.leap@student.cadt.edu.kh>
2024-01-23 21:38:46 +0700

    set up security environment for backend

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,21 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
-
+dotenv.config();
 const app = express();
 
-app.listen (4000 , ()=>{
-    console.log ('App is running on port 4000');
+
+
+mongoose.connect(process.env.DB_URI)
+    .then(() => {
+        console.log('database connect sucessfully');
+    })
+    .catch(error => {
+        console.log('It has been an error connected to the database', error);
+    })
+
+
+app.listen (process.env.PORT , ()=>{
+    console.log ('App is running on port' , process.env.PORT);
 })
