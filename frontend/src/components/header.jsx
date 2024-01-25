e86fe014e2b0753b4cf9a90e393d08741f08ccb9
Elon Thy <Chanvuthy.leap@student.cadt.edu.kh>
2024-01-25 18:30:59 +0700

    complete sign up page ui

--- frontend/src/components/header.jsx
+++ frontend/src/components/header.jsx
@@ -12,8 +12,8 @@ const Header = () => {
         to="/"
         className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white"
       >
-        <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
-          Sahand's
+        <span className="px-2 py-1 bg-gray-800 via-purple-500 to-pink-500 rounded-lg text-white">
+          Vuthy's
         </span>
         Blog
       </Link>
